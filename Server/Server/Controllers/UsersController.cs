f74b9f63100142f5e476395271ce43dc954ee714
Ofekbeg <110598292+Ofekbeg@users.noreply.github.com>
2024-04-02 14:19:38 +0300

    changed name space

--- Server/Server/Controllers/UsersController.cs
+++ Server/Server/Controllers/UsersController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics.Eventing.Reader;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace SongsServer.Controllers
+namespace Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
