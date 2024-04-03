f6feef5bae376cecfff24398466bdf1ffb9c4bb6
Ofekbeg <110598292+Ofekbeg@users.noreply.github.com>
2024-04-03 13:39:09 +0300

    Client

---
+++ Client/flone/src/App.js
@@ -0,0 +1,107 @@
+import { Suspense, lazy } from "react";
+import ScrollToTop from "./helpers/scroll-top";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// home page
+const HomeOrganicFood = lazy(() => import("./pages/home/HomeOrganicFood"));
+// shop pages
+const ShopGridStandard = lazy(() => import("./pages/shop/ShopGridStandard"));
+
+// product pages
+const Product = lazy(() => import("./pages/shop-product/Product"));
+const ProductTabLeft = lazy(() =>
+  import("./pages/shop-product/ProductTabLeft")
+);
+// other pages
+const About = lazy(() => import("./pages/other/About"));
+const Contact = lazy(() => import("./pages/other/Contact"));
+const MyAccount = lazy(() => import("./pages/other/MyAccount"));
+const LoginRegister = lazy(() => import("./pages/other/LoginRegister"));
+
+const Cart = lazy(() => import("./pages/other/Cart"));
+const Wishlist = lazy(() => import("./pages/other/Wishlist"));
+const Compare = lazy(() => import("./pages/other/Compare"));
+const Checkout = lazy(() => import("./pages/other/Checkout"));
+
+const NotFound = lazy(() => import("./pages/other/NotFound"));
+
+const App = () => {
+  return (
+      <Router>
+        <ScrollToTop>
+          <Suspense
+            fallback={
+              <div className="flone-preloader-wrapper">
+                <div className="flone-preloader">
+                  <span></span>
+                  <span></span>
+                </div>
+              </div>
+            }
+          >
+            <Routes>
+              
+              <Route
+                path={process.env.PUBLIC_URL + "/home-organic-food"}
+                element={<HomeOrganicFood/>}
+              />
+              
+              {/* Shop pages */}
+              <Route
+                path={process.env.PUBLIC_URL + "/shop-grid-standard"}
+                element={<ShopGridStandard/>}
+              />
+             
+              {/* Shop product pages */}
+              <Route
+                path={process.env.PUBLIC_URL + "/product/:id"}
+                element={<Product />}
+              />
+              <Route
+                path={process.env.PUBLIC_URL + "/product-tab-left/:id"}
+                element={<ProductTabLeft/>}
+              />
+              {/* Other pages */}
+              <Route
+                path={process.env.PUBLIC_URL + "/about"}
+                element={<About/>}
+              />
+              <Route
+                path={process.env.PUBLIC_URL + "/contact"}
+                element={<Contact/>}
+              />
+              <Route
+                path={process.env.PUBLIC_URL + "/my-account"}
+                element={<MyAccount/>}
+              />
+              <Route
+                path={process.env.PUBLIC_URL + "/login-register"}
+                element={<LoginRegister/>}
+              />
+
+              <Route
+                path={process.env.PUBLIC_URL + "/cart"}
+                element={<Cart/>}
+              />
+              <Route
+                path={process.env.PUBLIC_URL + "/wishlist"}
+                element={<Wishlist/>}
+              />
+              <Route
+                path={process.env.PUBLIC_URL + "/compare"}
+                element={<Compare/>}
+              />
+              <Route
+                path={process.env.PUBLIC_URL + "/checkout"}
+                element={<Checkout/>}
+              /> 
+
+              <Route path="*" element={<NotFound/>} />
+            </Routes>
+          </Suspense>
+        </ScrollToTop>
+      </Router>
+  );
+};
+
+export default App;
---
+++ Client/flone/src/components/header/NavMenu.js
@@ -0,0 +1,56 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import clsx from "clsx";
+
+const NavMenu = ({ menuWhiteClass, sidebarMenu }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div
+      className={clsx(
+        sidebarMenu
+          ? "sidebar-menu"
+          : `main-menu ${menuWhiteClass ? menuWhiteClass : ""}`
+      )}
+    >
+      <nav>
+        <ul>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/home-organic-food"}>
+              {t("Home")}
+            </Link>
+          </li>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+              {t("Shop")}
+            </Link>
+          </li>
+
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/about"}>{t("about_us")}</Link>
+          </li>
+          {/* <ul className="submenu">
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/not-found"}>
+                  {t("404_page")}
+                </Link>
+              </li>
+            </ul> */}
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/contact"}>
+              {t("contact_us")}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+NavMenu.propTypes = {
+  menuWhiteClass: PropTypes.string,
+  sidebarMenu: PropTypes.bool,
+};
+
+export default NavMenu;
