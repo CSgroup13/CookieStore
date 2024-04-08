7c02215dbb43181d834034e8713c8a84c532dae8
Ofekbeg <110598292+Ofekbeg@users.noreply.github.com>
2024-04-08 16:36:04 +0300

    fix navigate

--- Client/flone/src/pages/other/LoginRegister.js
+++ Client/flone/src/pages/other/LoginRegister.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useEffect, useState,useNavigate} from "react";
-import { useLocation } from "react-router-dom";
+import React, { Fragment, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import Tab from "react-bootstrap/Tab";
 import emailjs from "emailjs-com";
 import Nav from "react-bootstrap/Nav";
@@ -290,7 +290,9 @@ const LoginRegister = () => {
                         <div className="login-form-container">
                           <div className="login-register-form">
                             <form onSubmit={handleSubmit}>
-                              {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
+                              {errors.email && (
+                                <p style={{ color: "red" }}>{errors.email}</p>
+                              )}
                               <br />
                               <input
                                 type="text"
@@ -299,7 +301,11 @@ const LoginRegister = () => {
                                 value={loginData.email}
                                 onChange={handleChange}
                               />
-                              {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
+                              {errors.password && (
+                                <p style={{ color: "red" }}>
+                                  {errors.password}
+                                </p>
+                              )}
                               <br />
                               <input
                                 type="password"
@@ -361,7 +367,9 @@ const LoginRegister = () => {
                           <div className="login-register-form">
                             <form onSubmit={handleRegistrationSubmit}>
                               {registrationErrors.email && (
-                                <p style={{ color: "red" }}>{registrationErrors.email}</p>
+                                <p style={{ color: "red" }}>
+                                  {registrationErrors.email}
+                                </p>
                               )}
                               <br />
                               <input
@@ -372,7 +380,9 @@ const LoginRegister = () => {
                                 required
                               />
                               {registrationErrors.password && (
-                                <p style={{ color: "red" }}>{registrationErrors.password}</p>
+                                <p style={{ color: "red" }}>
+                                  {registrationErrors.password}
+                                </p>
                               )}
                               <br />
 
@@ -384,7 +394,9 @@ const LoginRegister = () => {
                                 required
                               />
                               {registrationErrors.confirmPassword && (
-                                <p style={{ color: "red" }}>{registrationErrors.confirmPassword}</p>
+                                <p style={{ color: "red" }}>
+                                  {registrationErrors.confirmPassword}
+                                </p>
                               )}
                               <br />
                               <input
