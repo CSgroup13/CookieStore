73a365275053ca9840e0b85e60af5dc9b2308f91
Ofekbeg <110598292+Ofekbeg@users.noreply.github.com>
2024-04-15 11:50:53 +0300

    paypal fix

--- Client/flone/src/pages/other/Checkout.js
+++ Client/flone/src/pages/other/Checkout.js
@@ -8,26 +8,15 @@ import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import { useState, useEffect } from "react";
-import api, { postData } from "../../utils/api.js";
-import cogoToast from "cogo-toast";
-import { useNavigate } from "react-router-dom";
-import emailjs from "emailjs-com";
-import { useDispatch } from "react-redux";
-import { deleteAllFromCart } from "../../store/slices/cart-slice";
-import { sub } from "date-fns";
-import { faker } from "@faker-js/faker";
-import { addNotification } from "../../store/slices/notifications-slice";
 import Paypal from "./PayPal";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 const Checkout = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
   let cartTotalPrice = 0;
   const initialOptions = {
     "client-id":
       "AXOHJgQSrZkQA98vO5RL3R-3g-mUSDbD_VR1N17zk-GUH4CdCVryLJsZDlk7DOArvvuF9cFUe9ziufET",
-    currency: "USD",
+    currency: "ILS",
     intent: "capture",
   };
   let { pathname } = useLocation();
@@ -110,7 +99,7 @@ const Checkout = () => {
     phone: loggedUser?.phone || "",
     email: loggedUser?.email || "",
     shipping: "",
-    payment: "",
+    payment: "Paypal",
     notes: "",
   });
   const [formErrors, setFormErrors] = useState({});
@@ -183,124 +172,9 @@ const Checkout = () => {
     });
   };
 
-  const placeOrder = () => {
-    if (isPlaceOrderValid) {
-      const orderItems = cartItems.map((item) => ({
-        productId: item.id,
-        quantity: item.quantity,
-      }));
-
-      const orderDetails = {
-        userId: loggedUser?.id || -1,
-        totalPrice: cartTotalPrice,
-        date: new Date(Date.now()),
-        shippingAddress: `${formData.address}, ${formData.apartment}, ${formData.city}, Israel, ${formData.postalCode}`,
-        shippingMethod: Number(formData.shipping),
-        paymentMethod: Number(formData.payment),
-        notes: formData.notes,
-        orderItems: orderItems,
-      };
-      postData(api.orders, orderDetails)
-        .then((order) => {
-          const orderDetailsMessage = `
-          New order received!
-            Order Number: ${order.id}
-            Total Price: ${order.totalPrice}
-            First Name: ${formData.firstName}
-            Last Name: ${formData.lastName}
-            Address: ${formData.address}
-            Apartment: ${formData.apartment}
-            City: ${formData.city}
-            Postal Code: ${formData.postalCode}
-            Phone: ${formData.phone}
-            Email: ${formData.email}
-            Shipping Method: ${
-              formData.shipping === "1" ? "Shipping" : "Pickup"
-            }
-            Payment Method: ${formData.payment === "1" ? "Bit App" : "Cash"}
-            Notes: ${formData.notes}
-            Order Items: ${cartItems
-              .map((item) => `${item.name}: ${item.quantity}`)
-              .join(", ")}
-            `;
-          // Sending confirmation email to the user
-          emailjs
-            .send(
-              "service_toin4ud",
-              "template_pi1v48b",
-              {
-                to_name: formData.firstName + " " + formData.lastName,
-                to_email: formData.email,
-                from_name: "Cookies Addiction",
-                message: `Your delicious cookies order (Order Number ${order.id}) has been successfully placed!🍪🎉 
-        Thank you for choosing us!`,
-              },
-              "Ov1O19nU4lvvYar64"
-            )
-            .then(() => {
-              // Notification to the user
-              cogoToast.success(`Your Order successfully placed!🍪🎉`, {
-                position: "top-right",
-              });
-              // Sending notification email to the admin
-              emailjs
-                .send(
-                  "service_toin4ud",
-                  "template_0134u4t",
-                  {
-                    to_name: "Shaked",
-                    to_email: "cookiesaddiction1@gmail.com",
-                    message: orderDetailsMessage,
-                  },
-                  "Ov1O19nU4lvvYar64"
-                )
-                .then(() => {
-                  console.log("Notification email sent to admin.");
-                  dispatch(deleteAllFromCart());
-                  const newNotification = {
-                    id: faker.string.uuid(),
-                    title: "You have new order",
-                    description: `from ${
-                      formData.firstName + " " + formData.lastName
-                    }`,
-                    avatar: null,
-                    type: "order_placed",
-                    createdAt: sub(new Date(), {
-                      days: 0,
-                      hours: 0,
-                      minutes: 0,
-                    }),
-                    isUnRead: false,
-                  };
-                  dispatch(addNotification(newNotification));
-                  navigate("/"); // Only navigate after sending the email to the admin
-                })
-                .catch((error) => {
-                  console.error("Error sending email to admin:", error);
-                  navigate("/"); // Handle error and navigate
-                });
-            })
-            .catch((error) => {
-              console.error("Error sending email to user:", error);
-              navigate("/"); // Handle error and navigate
-            });
-        })
-        .catch((error) => {
-          cogoToast.error(error.message, { position: "bottom-left" });
-        });
-    } else {
-      cogoToast.error("Form contains errors or missing information.", {
-        position: "bottom-left",
-      });
-    }
-  };
-
   return (
     <Fragment>
-      <SEO
-        titleTemplate="Checkout"
-        description="Checkout page of flone react minimalist eCommerce template."
-      />
+      <SEO titleTemplate="Checkout" />
       <LayoutOne headerTop="visible">
         {/* breadcrumb */}
         <Breadcrumb
@@ -472,32 +346,6 @@ const Checkout = () => {
                         )}
                       </div>
                     </div>
-                    <div className="col-lg-12">
-                      <div className="billing-info mb-20">
-                        <label>Payment Method</label>
-                        <select
-                          name="payment"
-                          onChange={handleChange}
-                          defaultValue={-1}
-                          required
-                        >
-                          <option value="-1" key="-1" disabled>
-                            choose payment method
-                          </option>
-                          <option value="1" key="1">
-                            Bit App
-                          </option>
-                          <option value="2" key="2">
-                            Cash
-                          </option>
-                        </select>
-                        {formErrors.payment && (
-                          <span style={{ color: "red" }}>
-                            {formErrors.payment}
-                          </span>
-                        )}
-                      </div>
-                    </div>
                     <div className="additional-info-wrap">
                       <h4>Additional information</h4>
                       <div className="additional-info">
@@ -581,12 +429,15 @@ const Checkout = () => {
                       <div className="payment-method"></div>
                     </div>
                     <div className="place-order mt-25">
-                      <button className="btn-hover" onClick={placeOrder}>
-                        Place Order
-                      </button>
-                      <PayPalScriptProvider options={initialOptions}>
-                        <Paypal />
-                      </PayPalScriptProvider>
+                      {isPlaceOrderValid && (
+                        <PayPalScriptProvider options={initialOptions}>
+                          <Paypal
+                            formData={formData}
+                            cartItems={cartItems}
+                            loggedUser={loggedUser}
+                          />
+                        </PayPalScriptProvider>
+                      )}
                     </div>
                   </div>
                 </div>
--- Client/flone/src/pages/other/PayPal.jsx
+++ Client/flone/src/pages/other/PayPal.jsx
@@ -1,14 +1,49 @@
 import React from "react";
 import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import api, { postData } from "src/utils/api";
+import cogoToast from "cogo-toast";
+import { useNavigate } from "react-router-dom";
+import emailjs from "emailjs-com";
+import { useDispatch } from "react-redux";
+import { deleteAllFromCart } from "src/store/slices/cart-slice";
+import { faker } from "@faker-js/faker";
+import { addNotification } from "src/store/slices/notifications-slice";
+import { sub } from "date-fns";
 
-const Paypal = () => {
+const Paypal = ({ formData, cartItems, loggedUser }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [{ isPending }] = usePayPalScriptReducer();
+  const { totalPrice, orderItems } = cartItems.reduce(
+    (accumulator, currentItem) => {
+      const itemPrice = currentItem.price * currentItem.quantity;
+      const orderItem = {
+        productId: currentItem.id,
+        quantity: currentItem.quantity,
+      };
+      accumulator.orderItems.push(orderItem);
+      accumulator.totalPrice += itemPrice;
+      return accumulator;
+    },
+    { totalPrice: 0, orderItems: [] }
+  );
+
+  const orderDetails = {
+    userId: loggedUser?.id || -1,
+    totalPrice,
+    date: new Date(Date.now()),
+    shippingAddress: `${formData.address}, ${formData.apartment}, ${formData.city}, Israel, ${formData.postalCode}`,
+    shippingMethod: Number(formData.shipping),
+    paymentMethod: Number(formData.payment),
+    notes: formData.notes,
+    orderItems: orderItems,
+  };
   const onCreateOrder = (data, actions) => {
     return actions.order.create({
       purchase_units: [
         {
           amount: {
-            value: "8.99",
+            value: totalPrice,
           },
         },
       ],
@@ -16,10 +51,104 @@ const Paypal = () => {
   };
 
   const onApproveOrder = (data, actions) => {
-    return actions.order.capture().then((details) => {
-      const name = details.payer.name.given_name;
-      alert(`Transaction completed by ${name}`);
-    });
+    return actions.order
+      .capture()
+      .then((details) => {
+        const name = details.payer.name.given_name;
+        postData(api.orders, orderDetails)
+          .then((order) => {
+            const orderDetailsMessage = `
+          New order received!
+            Order Number: ${order.id}
+            Total Price: ${order.totalPrice}
+            First Name: ${formData.firstName}
+            Last Name: ${formData.lastName}
+            Address: ${formData.address}
+            Apartment: ${formData.apartment}
+            City: ${formData.city}
+            Postal Code: ${formData.postalCode}
+            Phone: ${formData.phone}
+            Email: ${formData.email}
+            Shipping Method: ${
+              formData.shipping === "1" ? "Shipping" : "Pickup"
+            }
+            Payment Method: "Paypal"
+            Notes: ${formData.notes}
+            Order Items: ${cartItems
+              .map((item) => `${item.name}: ${item.quantity}`)
+              .join(", ")}
+            `;
+            // Sending confirmation email to the user
+            emailjs
+              .send(
+                "service_toin4ud",
+                "template_pi1v48b",
+                {
+                  to_name: formData.firstName + " " + formData.lastName,
+                  to_email: formData.email,
+                  from_name: "Cookies Addiction",
+                  message: `Your delicious cookies order (Order Number ${order.id}) has been successfully placed!🍪🎉 
+        Thank you for choosing us!`,
+                },
+                "Ov1O19nU4lvvYar64"
+              )
+              .then(() => {
+                // Notification to the user
+                cogoToast.success(
+                  `${name} Your Order successfully placed!🍪🎉`,
+                  {
+                    position: "top-right",
+                  }
+                );
+                // Sending notification email to the admin
+                emailjs
+                  .send(
+                    "service_toin4ud",
+                    "template_0134u4t",
+                    {
+                      to_name: "Shaked",
+                      to_email: "cookiesaddiction1@gmail.com",
+                      message: orderDetailsMessage,
+                    },
+                    "Ov1O19nU4lvvYar64"
+                  )
+                  .then(() => {
+                    dispatch(deleteAllFromCart());
+                    const newNotification = {
+                      id: faker.string.uuid(),
+                      title: "You have new order",
+                      description: `from ${
+                        formData.firstName + " " + formData.lastName
+                      }`,
+                      avatar: null,
+                      type: "order_placed",
+                      createdAt: sub(new Date(), {
+                        days: 0,
+                        hours: 0,
+                        minutes: 0,
+                      }),
+                      isUnRead: true,
+                    };
+                    dispatch(addNotification(newNotification));
+                    navigate("/"); // Only navigate after sending the email to the admin
+                  })
+                  .catch((error) => {
+                    console.error("Error sending email to admin:", error);
+                    navigate("/"); // Handle error and navigate
+                  });
+              })
+              .catch((error) => {
+                console.error("Error sending email to user:", error);
+                navigate("/"); // Handle error and navigate
+              });
+          })
+          .catch((error) => {
+            cogoToast.error(error.message, { position: "bottom-left" });
+          });
+      })
+      .catch((error) => {
+        cogoToast.error(error.message, { position: "bottom-left" });
+      });
   };
 
   return (
@@ -28,7 +157,7 @@ const Paypal = () => {
         <p>LOADING...</p>
       ) : (
         <PayPalButtons
-          style={{ layout: "vertical" }}
+          style={{ layout: "vertical", shape: "pill" }}
           createOrder={(data, actions) => onCreateOrder(data, actions)}
           onApprove={(data, actions) => onApproveOrder(data, actions)}
         />
