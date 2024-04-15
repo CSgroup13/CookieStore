e50c94cc9dcc5cf618d8a0806a17aa914ed5a7c7
Ofekbeg <110598292+Ofekbeg@users.noreply.github.com>
2024-04-15 18:24:44 +0300

    remove unnecessary shit

--- Client/flone/src/pages/other/About.js
+++ Client/flone/src/pages/other/About.js
@@ -1,14 +1,8 @@
 import { Fragment } from "react";
 import { useLocation } from "react-router-dom";
 import SEO from "../../components/seo";
-import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import SectionTitleWithText from "../../components/section-title/SectionTitleWithText";
-import BannerOne from "../../wrappers/banner/BannerOne";
-import TextGridOne from "../../wrappers/text-grid/TextGridOne";
-import FunFactOne from "../../wrappers/fun-fact/FunFactOne";
-import TeamMemberOne from "../../wrappers/team-member/TeamMemberOne";
-import BrandLogoSliderOne from "../../wrappers/brand-logo/BrandLogoSliderOne";
 import LayoutThree from "../../layouts/LayoutThree";
 
 const About = () => {
